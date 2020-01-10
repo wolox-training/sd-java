889b6fe16b9ad55186072fdaa37b80a0fb8db50f
Sebas <sebasdeldi123@gmail.com>
2020-01-10 16:13:05 -0500

    implement use of optionals

---
+++ src/main/java/com/wolox/training/Constant.java
@@ -0,0 +1,9 @@
+package com.wolox.training;
+
+public final class Constant {
+  private Constant(){}
+
+  public static final String BOOKNOTFOUND = "Book Not Found";
+  public static final String BOOKMISMATCH = "Book Id Mismatch";
+
+}
